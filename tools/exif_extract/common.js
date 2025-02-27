779d218edd92c096fb173ddcfaed9ab8f5cb18d1
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-02-27 14:36:34 +0100

    Create common.js

---
+++ tools/exif_extract/common.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import os from 'os'
+
+export const IMAGE_PATH_LOCATION = "./image_path.cfg"
+const FILES_DA_IGNORARE = ['.DS_Store', '.AppleDouble', '.LSOverride']
+
+export function getImagePath() {
+	try {
+		const path = fs.readFileSync(DEFAULT_CFG_PATH_LOCATION, 'utf8').trim()
+		const fixedPath = path.replace('~', os.homedir())
+		return fixedPath
+
+	} catch (e) {
+		console.log("Percorso immagine default non trovato/errato.")
+		console.log("Verifica il file " + DEFAULT_CFG_PATH_LOCATION)
+	}
+}
+
+export function scanFolder(path) {
+	return fs.readdirSync(path).filter( e => FILES_DA_IGNORARE.indexOf(e) == -1)
+}
