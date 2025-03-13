c4e31e5d0a5f9747b683385c01006392d88e2ab1
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-03-13 12:17:07 +0100

    Create sketch.js

---
+++ esempi/es05_p5js_script/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+
+	createCanvas(300, 200)
+	background(200)
+
+
+	fill(255, 255, 0)
+	rect(10, 20, 50, 50)
+
+	fill(255, 127, 0)
+	stroke(255)
+	rect(30, 30, 50, 50)
+
+
+
+	fill(255, 0, 0, 128)
+	noStroke()
+
+	ellipse(90, 50, 70, 70)
+
+}
+
+function draw() {
+
+}
+
