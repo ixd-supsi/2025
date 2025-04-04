08c0685944cb8e933ade49839ff6008fbcb573e5
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-04-04 12:23:42 +0200

    Update sketch.js

--- esempi/es05_p5js_script/sketch.js
+++ esempi/es05_p5js_script/sketch.js
@@ -1,26 +1,24 @@
 function setup() {
-
+	// Crea un'area di disegno e imposta lo sfondo
 	createCanvas(300, 200)
 	background(200)
 
-
+	// Disegna un rettangolo giallo
 	fill(255, 255, 0)
 	rect(10, 20, 50, 50)
 
+	// Disegna un rettangolo arancione con bordo bianco
 	fill(255, 127, 0)
 	stroke(255)
 	rect(30, 30, 50, 50)
 
-
-
+	// Disegna un cerchio rosso semitrasparente
 	fill(255, 0, 0, 128)
 	noStroke()
-
 	ellipse(90, 50, 70, 70)
-
 }
 
+// Funzione draw vuota - nessuna animazione
 function draw() {
-
 }
 
