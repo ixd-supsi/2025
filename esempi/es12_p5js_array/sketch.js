8b60ad1dcf4d020f9bef6c40517ce34d4277842b
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-04-04 12:24:19 +0200

    Update sketch.js

--- esempi/es12_p5js_array/sketch.js
+++ esempi/es12_p5js_array/sketch.js
@@ -1,21 +1,23 @@
-
-
+// Array per memorizzare le immagini del labirinto
 const immagini = []
 
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL)
+	// Carica 200 immagini del labirinto
 	for (let i=0; i<200; i++) {
 		immagini[i] = loadImage("maze/maze_" + nf(i, 3) + ".jpg")
 	}
 }
 
 function draw() {
-	const distanza = 2
+	const distanza = 2 // Spazio tra le immagini
 	background(0)
-	scale (1.4)
-	rotateY(frameCount % 800 / 800 * TAU)
+	scale(1.4)
+	rotateY(frameCount % 800 / 800 * TAU) // Rotazione continua
 	translate(0, 0, immagini.length / 2 * distanza)
 	imageMode(CENTER)
+
+	// Disegna le immagini in 3D con effetto tunnel
 	for (let i=0; i<immagini.length; i++) {
 		push()
 		translate(0, 0, -i * distanza)
@@ -24,6 +26,7 @@ function draw() {
 	}
 }
 
+// Ridimensiona il canvas quando cambia la finestra
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight)
 }
