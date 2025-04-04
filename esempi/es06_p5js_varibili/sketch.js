ba57a8b0439ed2aae38af082caeceaa00ab01da1
Andreas Gysin <andreas@ertdfgcvb.ch>
2025-04-04 12:23:51 +0200

    Update sketch.js

--- esempi/es06_p5js_varibili/sketch.js
+++ esempi/es06_p5js_varibili/sketch.js
@@ -1,15 +1,17 @@
-
 function setup() {
 	createCanvas(300, 200)
 	background(200)
 }
 
 function draw() {
+	// Colore basato sulla posizione del mouse
 	fill(mouseX, mouseY, 0)
+	// Disegna due cerchi speculari
 	ellipse(mouseX, mouseY, 30, 30)
 	ellipse(300 - mouseX, mouseY, 30, 30)
 }
 
+// Cambia lo sfondo con un colore casuale quando si preme un tasto
 function keyPressed() {
-	background(random(256), random(256), random(256) )
+	background(random(256), random(256), random(256))
 }
